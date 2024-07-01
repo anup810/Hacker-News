da8bc4be4b326ff7c54e4945a3828ffca1bf9d75
Anup Saud <anupsaud10@gmail.com>
2024-07-01 13:21:04 -0400

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Anup Saud on 2024-07-01.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
